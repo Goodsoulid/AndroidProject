5564b68d95b8514f458ab6cf8682b91ae7d8edd1
Goodsoulid <Frozon1994@gmail.com>
2022-12-12 19:21:23 +0300

    Lesson 23 (mvp,Clean Architecture,viewBinding,gradle module)

--- app/src/main/java/com/example/androidproject/ItemsFragment.kt
+++ app/src/main/java/com/example/androidproject/ItemsFragment.kt
@@ -1,19 +1,16 @@
 package com.example.androidproject
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.androidproject.BundleConstants.IMAGE_VIEW
 import com.example.androidproject.adapter.ItemsAdapter
 import com.example.androidproject.listener.ItemsListener
-import com.example.androidproject.model.ItemsModel
 
 //не использовать
 const val NAME = "name"
